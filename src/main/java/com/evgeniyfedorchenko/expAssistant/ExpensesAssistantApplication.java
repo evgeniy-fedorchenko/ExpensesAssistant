7fcb129b84195e2f0dd899975f24709e11bb3dc2
EvgeniyFedorchenko <jecky432@gmail.com>
2024-03-29 19:12:59 +0300

    Initial commit

---
+++ src/main/java/com/evgeniyfedorchenko/expAssistant/ExpensesAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.evgeniyfedorchenko.expAssistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesAssistantApplication.class, args);
+	}
+
+}
